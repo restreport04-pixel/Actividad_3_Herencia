70cc81cfb6bb3fff0179bc0d4410edf2a93813d2
Thomas Restrepo Rodríguez <restreport04@gmail.com>
2026-08-24 20:38:39 -0500

    Parte 2

---
+++ src/main/java/com/example/Transporte.java
@@ -0,0 +1,73 @@
+package com.example;
+
+public class Transporte {
+
+    // Atributos privados
+    private String idTransporte;
+    private double combustible;
+    private double capacidadCarga;
+
+    // CONSTRUCTOR 
+    public Transporte(String idTransporte, double combustible, double capacidadCarga){
+        this.idTransporte = idTransporte;
+        setCombustible(combustible);
+        setCapacidadCarga(capacidadCarga);
+    }
+
+
+    // GETTERS Y SETTERS
+    public String getIdTransporte(){
+        return idTransporte;
+    }
+
+    public void setIdTransporte(String idTransporte){
+        this.idTransporte = idTransporte;
+    }
+
+    public double getCombustible(){
+        return combustible;
+    }
+
+    public void setCombustible(double nivel){
+        if (nivel < 0)
+        {
+            this.combustible = 0;
+        }
+        else if (nivel > 100)
+        {
+            this.combustible = 100;
+        }
+        else 
+        {
+            this.combustible = nivel;
+        }
+    }
+
+    public double getCapacidadCarga(){
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(double carga){
+        if (carga < 0)
+        {
+            System.out.println("❌ Error: La capacidad de carga no puede ser negativa.");
+        }
+        else 
+        {
+            this.capacidadCarga = carga;
+        }
+    }
+
+    public void viajar(int distancia){
+        double consumo = distancia / 10.0;
+        if (this.combustible >= consumo)
+        {
+            this.combustible -= consumo;
+            System.out.println("🚚 El transporte " + idTransporte + " viajo " + distancia + " km. Combustible restante: " + this.combustible);
+        }
+        else 
+        {
+            System.out.println("❌ No hay suficiente combustible en " + idTransporte + " para viajar " + distancia + " km.");
+        }
+    }
+}
